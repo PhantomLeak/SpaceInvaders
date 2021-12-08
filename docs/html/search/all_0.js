e7b76737f57d4b6ba784ca2329547d6a7385bec4
Dylan Stocking <40648525+PhantomLeak@users.noreply.github.com>
2021-12-08 14:39:20 -0500

    Updating doxygen files

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['block_0',['Block',['../class_block.html',1,'Block'],['../class_block.html#a37658a946bf5067ad01d68d9ff086adc',1,'Block::Block()'],['../class_block.html#ae3d1015a2856d46334d960f912f3cb3e',1,'Block::Block(int x, int y, int width, int height, fgcugl::Color color=fgcugl::White)']]],
-  ['block_2eh_1',['Block.h',['../_block_8h.html',1,'']]],
-  ['bug_20list_2',['Bug List',['../bug.html',1,'']]]
+  ['block_2ecpp_1',['Block.cpp',['../_block_8cpp.html',1,'']]],
+  ['block_2eh_2',['Block.h',['../_block_8h.html',1,'']]],
+  ['bug_20list_3',['Bug List',['../bug.html',1,'']]]
 ];
